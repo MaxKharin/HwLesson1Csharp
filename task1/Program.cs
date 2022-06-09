6a72406c8bab50dc7babe266b213d1ae0578948c
MaximKharin <maximkharin@gmail.com>
2022-06-09 19:17:22 +0300

    finished first task

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter first number, please");
+string firstNum = Console.ReadLine();
+int first = int.Parse(firstNum);
+Console.WriteLine("Enter second number, please");
+string secondNum = Console.ReadLine();
+int second = int.Parse(secondNum);
+
+if (first > second)
+{
+    Console.WriteLine($"{first} is bigger than {second}");
+}
+
+else if(first == second)
+{
+    Console.WriteLine($"{first} is equal to {second}");
+}
+else
+{
+    Console.WriteLine($"{second} is bigger than {first}");
+}
