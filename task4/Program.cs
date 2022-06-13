fda5b4c8a27f020569a593f85677019a0ca65f08
MaximKharin <maximkharin@gmail.com>
2022-06-13 12:34:38 +0300

    finished task 4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter your number, please");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+Console.WriteLine("Your numbers are: ");
+
+for (int i = 1; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i}");
+    }
+}
